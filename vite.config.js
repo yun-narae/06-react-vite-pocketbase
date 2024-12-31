27fc709a1bb3a3b08605ee60450e1acb46166ad6
seulbinim <skfo0827@naver.com>
2024-12-31 11:11:19 +0900

    경로 수정..

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/06-react-vite-pocketbase/',
   css: {
     postcss: {
       plugins: [tailwindcss()],
