d5b86710dc67ce9079e849cc6e8ead09df65a362
seulbinim <skfo0827@naver.com>
2024-12-31 15:09:48 +0900

    수정

--- src/lib/getPbImageURL.js
+++ src/lib/getPbImageURL.js
@@ -1,4 +1,7 @@
-// 이미지 관련
-export default function getPbImageURL(item,fileName = 'field'){
-    return `${import.meta.env.VITE_PB_API}/files/${item.collectionId}/${item.id}/${item.field}`
-  }
+export default function getPbImageURL(item, fileName = 'field') {
+  const baseURL = import.meta.env.MODE === "development"
+      ? "http://127.0.0.1:8090/api" // 로컬 개발 환경
+      : import.meta.env.VITE_PB_API; // 배포 환경
+
+  return `${baseURL}/files/${item.collectionId}/${item.id}/${item.field}`;
+}
