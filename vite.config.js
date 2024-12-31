928881128b0022b79b9f8f43e50f74e0e0fbd1cd
seulbinim <skfo0827@naver.com>
2024-12-31 10:44:50 +0900

    base: '/06-react-vite-pocketbase/',

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-//   base: '/06-react-vite-pocketbase/',
+  base: '/06-react-vite-pocketbase/',
   css: {
     postcss: {
       plugins: [tailwindcss()],
