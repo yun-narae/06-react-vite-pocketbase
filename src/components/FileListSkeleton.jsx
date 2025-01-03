c07b85703ae87f3c5d5757736596b0dce314b38b
seulbinim <skfo0827@naver.com>
2025-01-03 15:55:48 +0900

    Skeleton 다크모드 스타일링

--- src/components/FileListSkeleton.jsx
+++ src/components/FileListSkeleton.jsx
@@ -8,26 +8,26 @@ const FileListSkeleton = () => {
                 {/* 업로드된 갯수만큼 */}
                 {/* {[...Array(8)].map((_, index) => (
                     <li key={index} className="cursor-pointer overflow-hidden">
-                        <figure className="rounded-xl overflow-hidden mb-2 flex items-center h-[180px] md:h-[240px] lg:h-[320px] skeleton" />
+                        <figure className="rounded-xl overflow-hidden mb-2 flex items-center h-[180px] md:h-[240px] lg:h-[320px] skeleton dark:skeleton-dark" />
                         <div className="flex flex-col">
-                            <span className="w-32 h-4 rounded skeleton" />
-                            <span className="w-24 h-4 rounded skeleton mt-2" />
+                            <span className="w-32 h-4 rounded skeleton dark:skeleton-dark" />
+                            <span className="w-24 h-4 rounded mt-2 skeleton dark:skeleton-dark" />
                         </div>
                     </li>
                 ))} */}
                 {/* 2개만 */}
                 <li className="cursor-pointer overflow-hidden">
-                        <figure className="rounded-xl overflow-hidden mb-2 flex items-center h-[180px] md:h-[240px] lg:h-[320px] skeleton" />
+                        <figure className="rounded-xl overflow-hidden mb-2 flex items-center h-[180px] md:h-[240px] lg:h-[320px] skeleton dark:skeleton-dark" />
                         <div className="flex flex-col">
-                            <span className="w-32 h-4 rounded skeleton" />
-                            <span className="w-24 h-4 rounded skeleton mt-2" />
+                            <span className="w-32 h-4 rounded skeleton dark:skeleton-dark" />
+                            <span className="w-24 h-4 rounded mt-2 skeleton dark:skeleton-dark" />
                         </div>
                 </li>
                 <li className="cursor-pointer overflow-hidden">
-                        <figure className="rounded-xl overflow-hidden mb-2 flex items-center h-[180px] md:h-[240px] lg:h-[320px] skeleton" />
+                        <figure className="rounded-xl overflow-hidden mb-2 flex items-center h-[180px] md:h-[240px] lg:h-[320px] skeleton dark:skeleton-dark" />
                         <div className="flex flex-col">
-                            <span className="w-32 h-4 rounded skeleton" />
-                            <span className="w-24 h-4 rounded skeleton mt-2" />
+                            <span className="w-32 h-4 rounded skeleton dark:skeleton-dark" />
+                            <span className="w-24 h-4 rounded mt-2 skeleton dark:skeleton-dark" />
                         </div>
                 </li>
             </ul>
--- tailwind.config.js
+++ tailwind.config.js
@@ -19,6 +19,7 @@ export default {
       },
       backgroundImage: {
         'gradient-custom': 'linear-gradient(to right, #ececec 0%, #f0f0f0 50%, #e6e6e6 100%)',
+        'gradient-custom-dark': 'linear-gradient(to right, #2d2d2d 0%, #444444 50%, #333333 100%)', // 다크 모드용 색상 (bg-gray-800과 비슷한 색상)
       },
       backgroundSize: {
         custom: '300% 100%',
