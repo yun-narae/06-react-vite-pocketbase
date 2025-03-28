756fc1a37b8e3e8e66251a8396757abb48108070
seulbinim <skfo0827@naver.com>
2025-03-28 13:16:46 +0900

    필드 이름 변경

--- src/pages/PostContent.jsx
+++ src/pages/PostContent.jsx
@@ -11,18 +11,18 @@ const PostContent = ({ commentCount, onClick, user, post, handleImageClick, isMo
             <div className="mb-2">
                 <p className="font-bold text-gray-700 mb-2">{post.editor}님</p>
                 <h2 className="text-xl font-bold">{post.title}</h2>
-                <p className="text-gray-700 mb-2">{post.text}</p>
+                <p className="text-gray-700 mb-2">{post.description}</p>
                 <p className="text-sm text-gray-500">
                     {new Date(post.updated).toISOString().split("T")[0]}
                 </p>
             </div>
 
             {/* 이미지 슬라이더 */}
-            {post.field && Array.isArray(post.field) ? (
+            {post.images && Array.isArray(post.images) ? (
                 <div onClick={onClick}>
                     {isMobile ? (
                         <Swiper navigation modules={[Navigation]}>
-                            {post.field.map((img, index) => (
+                            {post.images.map((img, index) => (
                                 <SwiperSlide key={index}>
                                     <img 
                                         src={`${import.meta.env.VITE_PB_API}/files/${post.collectionId}/${post.id}/${img}`} 
@@ -35,7 +35,7 @@ const PostContent = ({ commentCount, onClick, user, post, handleImageClick, isMo
                         </Swiper>
                     ) : (
                         <div className="flex space-x-2">
-                            {post.field.map((img, index) => (
+                            {post.images.map((img, index) => (
                                 <img
                                     key={index}
                                     src={`${import.meta.env.VITE_PB_API}/files/${post.collectionId}/${post.id}/${img}`}
